318f865c6eb2df7108c384b8195eb18c2a274790
atuki takata <ghuhumei@yahoo.co.jp>
2020-11-16 23:33:20 +0900

    neighbor naze

--- ROUTING/ERP/config.h
+++ ROUTING/ERP/config.h
@@ -27,38 +27,50 @@ struct network {
   struct prefix prefix;
 };
 
-static inline void
+static inline int
 config_parse(struct config* cfg, char* json)
 {
   char addr_str[256];
-  json_t* read_json_ob;
+  json_t *read_json_ob;
   json_error_t error;
   read_json_ob = json_load_file(json, 0, &error);
   if ( read_json_ob == NULL )
   {
         printf("NULL\n");
+	return 0;
   }
   printf("read done\n");
-  inet_aton(json_string_value(json_object_get(read_json_ob, "router-id")), 
-            &cfg->router_id);
+  int pton = inet_pton(AF_INET, json_string_value(json_object_get
+                      (read_json_ob, "router-id")), 
+                       &cfg->router_id);
+  printf("%d \n", pton);
   inet_ntop(AF_INET, &cfg->router_id, addr_str ,256);
   printf("router-id: %s\n", addr_str);
-  json_t* read_json_ob_neighbors;
-  read_json_ob_neighbors = json_object_get(
-	  read_json_ob, "neighbors");
-  if (read_json_ob_neighbors == NULL) {
+  /* ここからneighbors  */
+  for (size_t i = 0; i < MAX_NEIGH; i++) {
+    if (!cfg->networks[i])
+      continue;
+    json_t *json_nei;
+    json_nei = json_object_get(
+	    read_json_ob, "neighbors");
+    if (json_nei == NULL) {
 	  printf("NULL \n");
-  }
-  
-  int index;
-  json_t *read_json_ob_ipadress;
-  json_array_foreach(read_json_ob_neighbors, 
-	 index, read_json_ob_ipadress) {
-	 inet_aton(json_string_value(json_object_get(
-		   read_json_ob_ipadress, "address")),
-		   &cfg->neighbors[index]->address);
-    inet_ntop(AF_INET, &cfg->neighbors[index]->address, addr_str, 256);
-    printf("network[%d]: %s\n", index, addr_str);
-  }
+	  return 0;  
+    }
+    
+    int index;
+    json_t *json_neiaddr;
+    json_array_foreach(json_nei, index, json_neiaddr) {
+	    int pton = inet_pton(AF_INET, json_string_value
+		       (json_object_get(json_neiaddr, "address")), 
+			&cfg->neighbors[13]->address);
+			if (pton < 0) {
+			  printf("neighbors addr error");
+			}
+	    inet_ntop(AF_INET, &cfg->neighbors[13]->address, addr_str, 256);
+	    printf("neighbor[%d]: %s\n", i, addr_str);
+
+    }
 }
 
+}
--- ROUTING/ERP/libconfig.c
+++ ROUTING/ERP/libconfig.c
@@ -5,7 +5,11 @@
 int main()
 {
   struct config cfg;
-  config_parse(&cfg, "./setting.json");
+  int ret = config_parse(&cfg,"./setting.json");
+  if (ret < 0) {
+    fprintf(stderr, "failed on config parser\n");
+    return 1;
+  }
   
  // print_config(&cfg);
 }
