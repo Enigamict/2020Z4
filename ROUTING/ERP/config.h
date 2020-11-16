912d325a90a9aa3b43c2320fce290a09ee0d6f62
atuki takata <ghuhumei@yahoo.co.jp>
2020-11-17 00:10:39 +0900

    neighbor wakaranai

--- ROUTING/ERP/config.h
+++ ROUTING/ERP/config.h
@@ -34,43 +34,36 @@ config_parse(struct config* cfg, char* json)
   json_t *read_json_ob;
   json_error_t error;
   read_json_ob = json_load_file(json, 0, &error);
-  if ( read_json_ob == NULL )
+  if (read_json_ob == NULL )
   {
-        printf("NULL\n");
-	return 0;
+        printf("json configが読み取れません\n");
+	return -1;
   }
-  printf("read done\n");
-  int pton = inet_pton(AF_INET, json_string_value(json_object_get
-                      (read_json_ob, "router-id")), 
-                       &cfg->router_id);
-  printf("%d \n", pton);
+  inet_pton(AF_INET, json_string_value(json_object_get
+           (read_json_ob, "router-id")), 
+            &cfg->router_id);
   inet_ntop(AF_INET, &cfg->router_id, addr_str ,256);
   printf("router-id: %s\n", addr_str);
+  
   /* ここからneighbors  */
-  for (size_t i = 0; i < MAX_NEIGH; i++) {
-    if (!cfg->networks[i])
-      continue;
-    json_t *json_nei;
-    json_nei = json_object_get(
-	    read_json_ob, "neighbors");
-    if (json_nei == NULL) {
-	  printf("NULL \n");
-	  return 0;  
-    }
-    
-    int index;
-    json_t *json_neiaddr;
-    json_array_foreach(json_nei, index, json_neiaddr) {
-	    int pton = inet_pton(AF_INET, json_string_value
-		       (json_object_get(json_neiaddr, "address")), 
-			&cfg->neighbors[13]->address);
-			if (pton < 0) {
-			  printf("neighbors addr error");
-			}
-	    inet_ntop(AF_INET, &cfg->neighbors[13]->address, addr_str, 256);
-	    printf("neighbor[%d]: %s\n", i, addr_str);
+  json_t *json_nei;
+  json_nei = json_object_get(
+      read_json_ob, "neighbors");
+  if (json_nei == NULL) {
+    printf("neighborsの値を取得することができません \n");
+    return -1;  
+  }
 
-    }
+  int index;
+  json_t *json_neiaddr;
+  json_array_foreach(json_nei, index, json_neiaddr) {
+                     inet_pton(AF_INET, json_string_value
+		     (json_object_get(json_neiaddr, "address")), 
+		     &cfg->neighbors[13]->address);
+
+  inet_ntop(AF_INET, &cfg->neighbors[13]->address, addr_str, 256);
+  printf("neighbor[%d]: %s\n", index, addr_str);
+ }
 }
 
-}
+
